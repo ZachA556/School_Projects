887863e34ef60758218be70ead8cb8304879d23f
ZachA556 <101380007+ZachA556@users.noreply.github.com>
2022-06-14 10:13:36 -0400

    Add files via upload

---
+++ C#/arrayClassProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace arrayClassProject
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("How many numbers are there?");
+            string aSize = Console.ReadLine();
+            int i = Convert.ToInt32(aSize);
+            int [] numbers = new int [i];
+            
+            Console.WriteLine("Please enter numbers.");
+            double[] testNumbers = new double[numbers.Length];
+            for(int a = 0; a < numbers.Length; a++)
+            {
+                testNumbers[a] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine('\n');
+            for(int a = 0; a < numbers.Length; a++)
+            {
+                Array.Sort(testNumbers);
+                Console.WriteLine(testNumbers[a]);
+            }
+
+         }
+    }
+}
