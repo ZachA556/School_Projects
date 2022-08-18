5ee0ef99e1b3ffc4fca158cc03beeff1812eb7dd
ZachA556 <101380007+ZachA556@users.noreply.github.com>
2022-08-18 15:32:24 -0400

    Add files via upload

---
+++ things/Whatisgoingon/Form1.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Whatisgoingon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ConnectionString = "server = localhost; database = library; uid = root; pwd =; ";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection cnn = new MySqlConnection(ConnectionString);
+            cnn.Open();
+            MySqlDataAdapter MyDA = new MySqlDataAdapter();
+            string sqlSelectALL = "SELECT * from author";
+            MyDA.SelectCommand = new MySqlCommand(sqlSelectALL, cnn);
+
+            DataTable table = new DataTable();
+            MyDA.Fill(table);
+
+            BindingSource bSource = new BindingSource();
+            bSource.DataSource = table;
+
+            dataGridView1.DataSource = bSource;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
